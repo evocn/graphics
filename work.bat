bed6d35b21402078f222aa8d5dbf509e3d1c4631
Alex Hartford <alexanderhartford@gmail.com>
2025-12-13 18:47:38 -0800

    Return to form here.
    Updated to Jai 0.2.017 compiler.
    Added build checks for compiler version.
    Moved to a better set of Vulkan 1.4 bindings.
    Updated the way we do a lot of things on the new bindings.
    Worked our way through validation errors and got shader objects to
    compile correctly.

--- work.bat
+++ work.bat
@@ -1,4 +1,11 @@
 @echo off
+
+pushd "%~dp0"
+
+REM Windows Terminal with a cmd tab and a neovim tab
+start wt --profile "cmd" -d . cmd /k "jai build.jai" ; new-tab --profile "Neovim" -d . nvim -p "source/main.jai"
+
+REM Open the debugger
 start remedybg bin\debug.rdbg
-start gvim source/main.jai
-start wt.exe -d %~dp0
+
+popd
